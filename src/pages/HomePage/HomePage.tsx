bd2240cf557f9abe9172796484fce442ddc865b6
Josh <joshuasusak@gmail.com>
2023-06-04 18:22:48 +0100

    feat: create very rough homepage - First iteration, quickly developed to replace vite default

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,48 +1,52 @@
-import { Heading, Text, Center, SimpleGrid } from "@chakra-ui/react";
+import {
+  Heading,
+  Text,
+  Center,
+  SimpleGrid,
+  VStack,
+  Button,
+} from "@chakra-ui/react";
 import { TestComponent } from "../../components/TestComponent/TestComponent";
-import { useState } from "react";
-import reactLogo from "../../assets/react.svg";
-import viteLogo from "/vite.svg";
-import { Colours } from "../../colourScheme";
+//import { useState } from "react";
+//import { Colours } from "../../colourScheme";
 
 export const HomePage: React.FC = (): JSX.Element => {
-  const [count, setCount] = useState(0);
   return (
     <>
-      <Heading>HomePage</Heading>
+      <Center>
+        <VStack>
+          <Heading fontSize={"9xl"}>Watch your language!</Heading>
+          <Heading fontSize={"6xl"}>The future of AAC smartwatch apps.</Heading>
+          <Text fontSize={"lg"} maxW={"5xl"}>
+            Acting as the first co-designed AAC of its kind, researchers at
+            King's College London have collaborated closely with SLTs and
+            volunteers with Aphasia to develop 2 revolutionary smartwatch apps,
+            assisting in face-to-face interactions and private cognitive
+            support.
+          </Text>
+          <Text fontSize={"lg"} maxW="5xl">
+            The project is led by Mr. Humphrey Curtis and supervised by Dr.
+            Timothy Neate, alongside a wider team of academic and non-academic
+            partners.
+          </Text>
+        </VStack>
+      </Center>
 
-      <Heading>AACWatchApp (change colour in theme.ts)</Heading>
-      <Text>hi</Text>
       <TestComponent />
 
       <Center>
-        <SimpleGrid columns={2}>
-          <div>
-            <a href="https://vitejs.dev" target="_blank">
-              <img
-                src={viteLogo}
-                className="logo"
-                alt="Vite logo"
-                style={{
-                  backgroundColor: Colours.lightModeHeader,
-                }}
-              />
-            </a>{" "}
-          </div>
-          <div>
-            <a href="https://react.dev" target="_blank">
-              <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
-          </div>
+        <SimpleGrid columns={4} spacing={5}>
+          <Button>Project Description</Button>
+          <Button>What is Aphasia?</Button>
+          <Button>View Contributors</Button>
+          <Button>Send Feedback</Button>
         </SimpleGrid>
       </Center>
 
-      <Heading>AACWatchApp</Heading>
-      <div className="card">
-        <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <SimpleGrid columns={2} spacing={10} mt="2rem">
+        <Button height="40vh">Aphasia Phrases (Watch Out SVG)</Button>
+        <Button height="40vh">Aphasia Dialogue (Watch In SVG)</Button>
+      </SimpleGrid>
     </>
   );
 };
