deed04fa4200c510828af987db798539532afaca
Josh <joshuasusak@gmail.com>
2023-06-04 17:05:35 +0100

    chore: add correct colour scheme to theme

--- src/colourScheme.ts
+++ src/colourScheme.ts
@@ -1,8 +1,8 @@
 
 //Dictionary containing declarations for all colours used in the site.
 //By keeping it in one file, colours are not hard-coded and can instead be referenced with a variable.
-export const Colours : {[key: string]: string} = {
-    "lightModeHeader": "#1E2328",
-    "lightModeSecondary": "#ff0000",
-    "lightModeMuted": "#00ff00"
-}
+export const Colours: { [key: string]: string } = {
+  lightModeHeader: "#1E2328",
+  lightModeSecondary: "#333",
+  lightModeMuted: "#00ff00",
+};
