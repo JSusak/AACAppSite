a519cbd921911ed018bd9742cdb8e907ab53031f
Josh <joshuasusak@gmail.com>
2023-06-10 09:53:57 +0100

    refactor: improve appearance of design icon

--- src/pages/AboutPage/components/InteractiveDesign/InteractiveDesignIcon.tsx
+++ src/pages/AboutPage/components/InteractiveDesign/InteractiveDesignIcon.tsx
@@ -19,7 +19,9 @@ export const InteractiveDesignIcon = ({
       backgroundColor={"orange.100"}
       width={`${width.toString()}px`}
       height={`${height.toString()}px`}
-      borderRadius={"50%"}
+      borderRadius={"20%"}
+      borderColor="blue.100"
+      borderWidth={"5px"}
       display={"flex"}
       justifyContent={"center"}
       boxShadow={"2px 12px 20px rgba(0,0,0,0.2);"}
@@ -28,12 +30,18 @@ export const InteractiveDesignIcon = ({
         <VStack>
           <Image
             src="./kcl.png"
-            borderRadius={"50%"}
+            borderRadius={"20%"}
             fallbackSrc={`https://via.placeholder.com/${width / 2}x${
               height / 2
             }`}
           />
-          <Heading fontSize={"xl"} textAlign={"center"}>
+
+          <Heading
+            fontSize={["8px", "13px", "13px", "15px"]}
+            textAlign={"center"}
+            maxW="100%"
+            padding={2}
+          >
             {text}
           </Heading>
         </VStack>
