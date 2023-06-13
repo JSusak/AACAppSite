b928c9e65cad7053b6fbe428787f7f52d12a6e03
Josh <joshuasusak@gmail.com>
2023-06-13 11:01:42 +0100

    feat: add animations to app features

--- src/pages/WatchOutPage/components/AppFeature/AppFeature.tsx
+++ src/pages/WatchOutPage/components/AppFeature/AppFeature.tsx
@@ -4,6 +4,29 @@ import { Text } from "@chakra-ui/layout";
 import { ListItem, UnorderedList } from "@chakra-ui/react";
 import { Colours } from "../../../../colourScheme";
 import { useEffect } from "react";
+import { motion } from "framer-motion";
+
+const appFeatureAnim = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+
+    transition: {
+      staggerChildren: 0.2,
+      duration: 2,
+    },
+  },
+};
+
+const appFeatureSectionAnim = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.3,
+    },
+  },
+};
 
 export const AppFeature = ({
   featureName,
@@ -30,73 +53,83 @@ export const AppFeature = ({
     );
   });
   return (
-    <Center>
-      <Box>
-        <Heading
-          fontSize={"6xl"}
-          textAlign={isOnLeft ? "left" : "right"}
-          m="2rem"
-        >
-          {featureName}
-        </Heading>
-        <Center>
-          <Text textAlign={"left"} fontSize={"xl"} maxW="3xl" mb="2rem">
-            {featureDescription}
-          </Text>
-        </Center>
-        <Divider borderColor={Colours.lightModeHeader} />
-        <Center>
-          <Heading fontSize={"2xl"} m="2rem">
-            Numeric Fact{" "}
+    <motion.div
+      variants={appFeatureAnim}
+      initial="hidden"
+      whileInView="visible"
+    >
+      <Center>
+        <Box>
+          <motion.span variants={appFeatureSectionAnim}>
             <Heading
-              as="span"
-              color={Colours.lightModeWatchOutAccent}
-              fontSize={"2xl"}
+              fontSize={"6xl"}
+              textAlign={isOnLeft ? "left" : "right"}
+              m="2rem"
             >
-              1
-            </Heading>{" "}
-            | Numeric Fact{" "}
-            <Heading
-              as="span"
-              color={Colours.lightModeWatchOutAccent}
-              fontSize={"2xl"}
-            >
-              2
-            </Heading>{" "}
-            | Numeric Fact{" "}
-            <Heading
-              as="span"
-              color={Colours.lightModeWatchOutAccent}
-              fontSize={"2xl"}
-            >
-              3
-            </Heading>{" "}
+              {featureName}
+            </Heading>
+          </motion.span>
+          <motion.span variants={appFeatureSectionAnim}>
+            <Center>
+              <Text textAlign={"left"} fontSize={"xl"} maxW="3xl" mb="2rem">
+                {featureDescription}
+              </Text>
+            </Center>
+          </motion.span>
+          <Divider borderColor={Colours.lightModeHeader} />
+          <Center>
+            <Heading fontSize={"2xl"} m="2rem">
+              Numeric Fact{" "}
+              <Heading
+                as="span"
+                color={Colours.lightModeWatchOutAccent}
+                fontSize={"2xl"}
+              >
+                1
+              </Heading>{" "}
+              | Numeric Fact{" "}
+              <Heading
+                as="span"
+                color={Colours.lightModeWatchOutAccent}
+                fontSize={"2xl"}
+              >
+                2
+              </Heading>{" "}
+              | Numeric Fact{" "}
+              <Heading
+                as="span"
+                color={Colours.lightModeWatchOutAccent}
+                fontSize={"2xl"}
+              >
+                3
+              </Heading>{" "}
+            </Heading>
+          </Center>
+          <Divider borderColor={Colours.lightModeHeader} />
+
+          <Heading fontSize={"5xl"} m="2rem" textAlign={"center"}>
+            Useful when...
           </Heading>
-        </Center>
-        <Divider borderColor={Colours.lightModeHeader} />
 
-        <Heading fontSize={"5xl"} m="2rem" textAlign={"center"}>
-          Useful when...
-        </Heading>
+          <Center>
+            <UnorderedList fontSize={"xl"}>
+              {usefulWhen.map((useful, key) => {
+                return <ListItem key={key}>{useful}</ListItem>;
+              })}
+            </UnorderedList>
+          </Center>
 
-        <Center>
-          <UnorderedList fontSize={"xl"}>
-            {usefulWhen.map((useful, key) => {
-              return <ListItem key={key}>{useful}</ListItem>;
-            })}
-          </UnorderedList>
-        </Center>
-
-        <Heading
-          fontSize={"xl"}
-          mt="2rem"
-          textAlign={isOnLeft ? "left" : "right"}
-          color={Colours.lightModeWatchOutAccent}
-        >
-          {catchyLine}
-        </Heading>
-        <Divider borderColor={Colours.lightModeHeader} />
-      </Box>
-    </Center>
+          <Heading
+            fontSize={"xl"}
+            mt="2rem"
+            textAlign={isOnLeft ? "left" : "right"}
+            color={Colours.lightModeWatchOutAccent}
+          >
+            {catchyLine}
+          </Heading>
+          <Divider borderColor={Colours.lightModeHeader} />
+        </Box>
+      </Center>
+    </motion.div>
   );
 };
