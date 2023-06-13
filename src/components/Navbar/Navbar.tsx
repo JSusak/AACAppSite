3131635ad60815130b978702a110279a9a7563ac
Josh <joshuasusak@gmail.com>
2023-06-13 11:56:00 +0100

    feat: create sticky navbar - add user scroll detection - add scroll animations

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -10,10 +10,50 @@ import {
 import { Colours } from "../../colourScheme";
 import { NavbarLink } from "./NavbarLink";
 import { NavLink as RouterLink } from "react-router-dom";
+import { motion } from "framer-motion";
+import { useState, useEffect } from "react";
+
+const navbarAnims = {
+  hidden: {
+    opacity: 0.8,
+    translateY: -55,
+  },
+  active: {
+    opacity: 1,
+    translateY: 0,
+  },
+};
 
 export const Navbar: React.FC = (): JSX.Element => {
+  const [scrollPos, setScrollPos] = useState(0);
+  const [isScrollDown, setIsScrollDown] = useState(false);
+  //Determines if the user is scrolling down or up. Checks current window scroll position to
+  //the last recorded scroll position - If it is greater it means the user has scrolled down
+  //and therefore the navbar should be hidden.
+  const checkScrollPos = (): void => {
+    window.scrollY > scrollPos ? setIsScrollDown(true) : setIsScrollDown(false);
+    setScrollPos(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", checkScrollPos);
+
+    return () => {
+      window.removeEventListener("scroll", checkScrollPos);
+    };
+  });
+
   return (
-    <>
+    <motion.div
+      style={{
+        position: "sticky",
+        top: "0px",
+      }}
+      variants={navbarAnims}
+      animate={isScrollDown ? "hidden" : "active"}
+      transition={{ duration: 0.5, type: "tween" }}
+      whileHover={{ opacity: 0.9 }}
+    >
       <Box
         w="100%"
         backgroundColor={Colours.lightModeMainCol}
@@ -21,6 +61,8 @@ export const Navbar: React.FC = (): JSX.Element => {
         boxShadow={
           "rgba(0, 0, 0, 0.2) 0px 10px 30px -10px, rgba(0, 0, 0, 0.04) 0px 10px 10px -5px"
         }
+        zIndex={1000}
+        scrollBehavior={"smooth"}
       >
         <Container minW={"100%"} display={"flex"} alignItems={"center"} h={5}>
           <HStack alignItems={"center"}>
@@ -55,13 +97,13 @@ export const Navbar: React.FC = (): JSX.Element => {
             >
               <NavbarLink linkText="About" linkTo="about" />
               <NavbarLink linkText="People" linkTo="people" />
+              <NavbarLink linkText="Co-Design" linkTo="contact" />
               <NavbarLink linkText="Watch In" linkTo="watchin" />
               <NavbarLink linkText="Watch Out" linkTo="watchout" />
-              <NavbarLink linkText="Contact" linkTo="contact" />
             </HStack>
           </HStack>
         </Container>
       </Box>
-    </>
+    </motion.div>
   );
 };
