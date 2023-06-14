8dafa2ca601fd3b70580ea5136691945843b876e
Josh <joshuasusak@gmail.com>
2023-06-14 08:25:23 +0100

    feat: create download embed - links to a file for users to download - create separate download grid

---
+++ src/pages/WatchOutPage/components/DownloadEmbed/DownloadEmbed.tsx
@@ -0,0 +1,66 @@
+import {
+  Card,
+  CardBody,
+  CardHeader,
+  Center,
+  Heading,
+  Link,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { motion } from "framer-motion";
+
+const downloadVariants = {
+  visible: {
+    opacity: 1,
+    y: 0,
+    x: 0,
+    transition: {
+      type: "spring",
+    },
+  },
+  hidden: {
+    opacity: 0,
+    y: -50,
+    x: -25,
+  },
+};
+
+//In order to link to a download, the file should be available in the 'public'
+//folder which you can then pass in as a parameter, such as './appstore.svg'
+export const DownloadEmbed = ({
+  embedIcon,
+  embedName,
+  embedText,
+  embedLink,
+}: {
+  embedIcon: JSX.Element;
+  embedName: string;
+  embedText: string;
+  embedLink: string;
+}): JSX.Element => {
+  return (
+    <motion.div
+      whileHover={{ scale: 1.05, textDecoration: "none" }}
+      variants={downloadVariants}
+    >
+      <Link download href={embedLink}>
+        <Card w="100%">
+          <CardHeader>
+            <VStack>
+              {embedIcon}
+              <Heading size="lg">{embedName}</Heading>
+            </VStack>
+          </CardHeader>
+          <CardBody>
+            <Center>
+              <Text maxW="md" mt="-0.5rem">
+                {embedText}
+              </Text>
+            </Center>
+          </CardBody>
+        </Card>
+      </Link>
+    </motion.div>
+  );
+};
---
+++ src/pages/WatchOutPage/components/DownloadGrid/DownloadGrid.tsx
@@ -0,0 +1,76 @@
+import { SimpleGrid } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import { DownloadEmbed } from "../DownloadEmbed/DownloadEmbed";
+import { Colours } from "../../../../colourScheme";
+import {
+  FaAccessibleIcon,
+  FaAudioDescription,
+  FaImages,
+  FaPaperclip,
+} from "react-icons/fa";
+
+const downloadGrid = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      delayChildren: 0.7,
+      staggerChildren: 0.17,
+    },
+  },
+};
+
+export const DownloadGrid = (): JSX.Element => {
+  return (
+    <motion.div variants={downloadGrid} initial="hidden" whileInView="visible">
+      <SimpleGrid columns={[1, 1, 1, 2]} spacing={"1rem"} mt="2rem">
+        <DownloadEmbed
+          embedIcon={
+            <FaPaperclip color={Colours.lightModeWatchOutAccent} size="40px" />
+          }
+          embedName={"Download Material 1"}
+          embedText={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          }
+          embedLink={"test"}
+        />
+        <DownloadEmbed
+          embedIcon={
+            <FaImages color={Colours.lightModeWatchOutAccent} size="40px" />
+          }
+          embedName={"Download Material 2"}
+          embedText={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          }
+          embedLink={"test"}
+        />
+        <DownloadEmbed
+          embedIcon={
+            <FaAccessibleIcon
+              color={Colours.lightModeWatchOutAccent}
+              size="40px"
+            />
+          }
+          embedName={"Download Material 3"}
+          embedText={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          }
+          embedLink={"test"}
+        />
+        <DownloadEmbed
+          embedIcon={
+            <FaAudioDescription
+              color={Colours.lightModeWatchOutAccent}
+              size="40px"
+            />
+          }
+          embedName={"Download Material 4"}
+          embedText={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          }
+          embedLink={"test"}
+        />
+      </SimpleGrid>
+    </motion.div>
+  );
+};
