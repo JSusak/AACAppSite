215a971b1863fdef9cc9c3b6f63675244bb95cdf
Josh <joshuasusak@gmail.com>
2023-06-14 13:20:27 +0100

    refactor: add responsive styles to watch out hero

--- src/pages/WatchOutPage/components/WatchOutHero/WatchOutHero.tsx
+++ src/pages/WatchOutPage/components/WatchOutHero/WatchOutHero.tsx
@@ -4,19 +4,25 @@ import { motion } from "framer-motion";
 import { Image } from "@chakra-ui/react";
 import { Colours } from "../../../../colourScheme";
 import { FaGithub } from "react-icons/fa";
+import { ResponsiveSizes } from "../../../../responsiveSizes";
 
 export const WatchOutHero = (): JSX.Element => {
   return (
     <Grid minH="93vh" position="relative">
-      <VStack display={"flex"} justifyContent={"center"} alignItems="center">
+      <VStack
+        display={"flex"}
+        justifyContent={"center"}
+        alignItems="center"
+        textAlign={"center"}
+      >
         {" "}
         <WatchOutLogoAnim />
-        <Heading fontSize={"9xl"}>Watch Out.</Heading>
-        <Heading fontSize={"6xl"}>
+        <Heading fontSize={ResponsiveSizes.HeroHeaderSizes}>Watch Out.</Heading>
+        <Heading fontSize={ResponsiveSizes.SecondaryHeaderSizes}>
           Supporting{" "}
           <Heading
             as="span"
-            fontSize={"6xl"}
+            fontSize={ResponsiveSizes.SecondaryHeaderSizes}
             color={Colours.lightModeWatchOutAccent}
           >
             face-to-face
@@ -37,7 +43,13 @@ export const WatchOutHero = (): JSX.Element => {
             whileHover={{ scale: 1.3 }}
             transition={{ type: "spring" }}
           >
-            <Link>
+            <Link
+              _hover={{
+                bg: "none",
+                borderColor: "none",
+                color: Colours.lightModeNavColHighlight,
+              }}
+            >
               <FaGithub size="60px" />
             </Link>
           </motion.div>
