d3a31e6e0fd88a8bc7b8da6253af70ecc3554a86
Josh <joshuasusak@gmail.com>
2023-06-14 12:24:02 +0100

    refactor: add fade in animation to video embed

--- src/pages/WatchOutPage/components/VideoEmbed/VideoEmbed.tsx
+++ src/pages/WatchOutPage/components/VideoEmbed/VideoEmbed.tsx
@@ -1,13 +1,37 @@
 import { Box } from "@chakra-ui/react";
 import ReactPlayer from "react-player";
+import { motion } from "framer-motion";
+
+const videoEmbedVariants = {
+  visible: {
+    opacity: 1,
+
+    transition: {
+      duration: 3,
+    },
+  },
+  hidden: {
+    opacity: 0,
+  },
+};
 
 export const VideoEmbed = (): JSX.Element => {
   return (
-    <Box borderWidth={"3px"} borderColor={"blue.100"} width={"90%"}>
-      <ReactPlayer
-        url="https://www.youtube.com/watch?v=C_sb2KYbi-4"
-        width={"100%"}
-      />
-    </Box>
+    <motion.div
+      variants={videoEmbedVariants}
+      initial="hidden"
+      whileInView={"visible"}
+      style={{
+        width: "90%",
+      }}
+    >
+      <Box borderWidth={"3px"} borderColor={"blue.100"}>
+        <ReactPlayer
+          url="https://www.youtube.com/watch?v=C_sb2KYbi-4"
+          width={"100%"}
+          height={"700px"}
+        />
+      </Box>
+    </motion.div>
   );
 };
