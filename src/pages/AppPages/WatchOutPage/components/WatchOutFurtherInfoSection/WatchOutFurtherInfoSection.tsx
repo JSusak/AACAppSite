6b5620971739b002b2ebc290d01a06080865facd
Josh <joshuasusak@gmail.com>
2023-06-16 12:42:15 +0100

    refactor: change name of watch out further info - delete old download grid and further info section

---
+++ src/pages/AppPages/WatchOutPage/components/WatchOutFurtherInfoSection/WatchOutFurtherInfoSection.tsx
@@ -0,0 +1,81 @@
+import { Grid, Heading, VStack } from "@chakra-ui/react";
+import { VideoEmbed } from "../../../components/VideoEmbed/VideoEmbed";
+
+import { DownloadGrid } from "../../../components/DownloadGrid/DownloadGrid";
+import { DownloadEmbed } from "../../../components/DownloadEmbed/DownloadEmbed";
+import {
+  FaAccessibleIcon,
+  FaAudioDescription,
+  FaImages,
+  FaPaperclip,
+} from "react-icons/fa";
+import { Colours } from "../../../../../colourScheme";
+
+export const WatchOutFurtherInfoSection = (): JSX.Element => {
+  return (
+    <Grid minH="100vh" position="relative">
+      <VStack mt="2rem" mb="2rem">
+        <Heading fontSize={"6xl"}>Further Info...</Heading>
+        <VideoEmbed link="https://www.youtube.com/watch?v=SNsdeS8VKg4" />
+        <DownloadGrid
+          children={
+            <>
+              <DownloadEmbed
+                embedIcon={
+                  <FaPaperclip
+                    color={Colours.lightModeWatchOutAccent}
+                    size="40px"
+                  />
+                }
+                embedName={"Download Material 1"}
+                embedText={
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                }
+                embedLink={"test"}
+              />
+              <DownloadEmbed
+                embedIcon={
+                  <FaImages
+                    color={Colours.lightModeWatchOutAccent}
+                    size="40px"
+                  />
+                }
+                embedName={"Download Material 2"}
+                embedText={
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                }
+                embedLink={"test"}
+              />
+              <DownloadEmbed
+                embedIcon={
+                  <FaAccessibleIcon
+                    color={Colours.lightModeWatchOutAccent}
+                    size="40px"
+                  />
+                }
+                embedName={"Download Material 3"}
+                embedText={
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                }
+                embedLink={"test"}
+              />
+              <DownloadEmbed
+                embedIcon={
+                  <FaAudioDescription
+                    color={Colours.lightModeWatchOutAccent}
+                    size="40px"
+                  />
+                }
+                embedName={"Download Material 4"}
+                embedText={
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                }
+                embedLink={"test"}
+              />
+            </>
+          }
+        />
+      </VStack>
+    </Grid>
+  );
+};
