b63d9ba1c6612fe6987ba8c73e88241af4ab339f
Josh <joshuasusak@gmail.com>
2023-06-20 09:57:21 +0100

    refactor: import array and loop for contents

--- src/pages/CoDesignPage/components/TimelineSection/TimelineSection.tsx
+++ src/pages/CoDesignPage/components/TimelineSection/TimelineSection.tsx
@@ -2,6 +2,7 @@ import { Box, Center, Container, Heading, Text } from "@chakra-ui/react";
 import "./timelinesection.scss";
 
 import { TimelineBeadSection } from "./TimelineBeadSection";
+import { timelineCardContents } from "./timelineCardContents";
 
 export const TimelineSection = (): JSX.Element => {
   return (
@@ -28,54 +29,21 @@ export const TimelineSection = (): JSX.Element => {
             <div className="circle beadTip" id="beadstart"></div>
           </Center>
 
-          <TimelineBeadSection
-            orderInTimeline={1}
-            isOnLeft={true}
-            cardImageURL={""}
-            cardName={"Workshop 1"}
-            modalDescription={
-              "Modal description for workshop 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            }
-            activityLocation={"Bush House"}
-            activityDate={"Sep. 2022"}
-            taskList={["Workshop activity 1", "workshop activity 2"]}
-          />
-          <TimelineBeadSection
-            orderInTimeline={2}
-            isOnLeft={false}
-            cardImageURL={""}
-            cardName={"Workshop 2"}
-            modalDescription={
-              "Modal description for workshop 2. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            }
-            activityLocation={"Bush House"}
-            activityDate={"Oct. 2022"}
-            taskList={["Workshop activity 1", "workshop activity 2"]}
-          />
-          <TimelineBeadSection
-            orderInTimeline={3}
-            isOnLeft={true}
-            cardImageURL={""}
-            cardName={"Workshop 3"}
-            modalDescription={
-              "Modal description for workshop 3. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            }
-            activityLocation={"Bush House"}
-            activityDate={"Dec. 2022"}
-            taskList={["Workshop activity 1", "workshop activity 2"]}
-          />
-          <TimelineBeadSection
-            orderInTimeline={4}
-            isOnLeft={false}
-            cardImageURL={""}
-            cardName={"Workshop 4"}
-            modalDescription={
-              "Modal description for workshop 4. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            }
-            activityLocation={"Bush House"}
-            activityDate={"Feb. 2023"}
-            taskList={["Workshop activity 1", "workshop activity 2"]}
-          />
+          {timelineCardContents.map((card, index) => {
+            return (
+              <TimelineBeadSection
+                orderInTimeline={index}
+                isOnLeft={index % 2 === 0}
+                cardImageURL={card.cardImageURL}
+                cardName={card.cardName}
+                modalDescription={card.modalDescription}
+                activityLocation={card.activityLocation}
+                activityDate={card.activityDate}
+                taskList={card.activityTasks}
+              />
+            );
+          })}
+
           <Center>
             <div className="circle beadTip"></div>
           </Center>
