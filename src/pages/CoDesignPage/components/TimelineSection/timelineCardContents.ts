58ff4623f96643648fb7f741fb2be7cc2c5efbd9
Josh <joshuasusak@gmail.com>
2023-06-20 09:56:13 +0100

    refactor: create central array of dicts for cards

---
+++ src/pages/CoDesignPage/components/TimelineSection/timelineCardContents.ts
@@ -0,0 +1,36 @@
+//Use this file to add/edit/delete all timeline sections. Keeps data in one place!
+//Each array entry should be a dictionary. You can just copy and paste any existing dictionary for it to work.
+export const timelineCardContents = [
+  //workshop 1,
+  {
+    cardImageURL: "./dialogue.png",
+    cardName: "Workshop 1",
+    modalDescription:
+      "This is the text for the modal description of workshop 1.",
+    activityLocation: "Bush House",
+    activityDate: "Oct. 2022",
+    activityTasks: ["task1", "task2", "task3"],
+  },
+
+  //workshop 2,
+  {
+    cardImageURL: "./phrases.png",
+    cardName: "Workshop 2",
+    modalDescription:
+      "This is the text for the modal description of workshop 2.",
+    activityLocation: "Bush House",
+    activityDate: "Nov. 2022",
+    activityTasks: ["task1", "task2", "task3"],
+  },
+
+  //workshop 3,
+  {
+    cardImageURL: "./dialogue.png",
+    cardName: "Workshop 3",
+    modalDescription:
+      "This is the text for the modal description of workshop 3.",
+    activityLocation: "Bush House",
+    activityDate: "Dec. 2022",
+    activityTasks: ["task1", "task2", "task3"],
+  },
+];
