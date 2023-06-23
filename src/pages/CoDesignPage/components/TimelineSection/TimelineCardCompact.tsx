9d669aa029507ee6e73ff715587fbffec2bcb7cf
Josh <joshuasusak@gmail.com>
2023-06-23 13:32:55 +0100

    feat: create first rendition of timeline compact

--- src/pages/CoDesignPage/components/TimelineSection/TimelineCardCompact.tsx
+++ src/pages/CoDesignPage/components/TimelineSection/TimelineCardCompact.tsx
@@ -1,4 +1,12 @@
-import { Heading } from "@chakra-ui/react";
+import {
+  Card,
+  Heading,
+  Image,
+  VStack,
+  Text,
+  ListItem,
+  UnorderedList,
+} from "@chakra-ui/react";
 
 export const TimelineCardCompact = ({
   id,
@@ -8,19 +16,45 @@ export const TimelineCardCompact = ({
   activityLocation,
   activityDate,
   taskList,
+  onImageLoad,
 }: {
-  id: number;
+  id: string;
   cardImageURL: string;
   cardName: string;
   modalDescription: string;
   activityLocation: string;
   activityDate: string;
   taskList: string[];
+  onImageLoad: () => void;
 }): JSX.Element => {
   return (
-    <Heading>
-      {id},{cardImageURL},{cardName},{modalDescription},{activityLocation},
-      {activityDate},{taskList}
-    </Heading>
+    <Card id={id} width={"90%"}>
+      <Image
+        onLoad={onImageLoad}
+        src={cardImageURL}
+        width={"100%"}
+        height="300px"
+        borderRadius={"10px"}
+        fallbackSrc={`https://via.placeholder.com/400x400`}
+        objectFit={"cover"}
+      />
+      <VStack p="1rem">
+        <Heading fontSize={"5xl"}>{`${cardName}`}</Heading>
+        <Text fontSize={"3xl"}>{`${activityLocation} • ${activityDate}`}</Text>
+        <Heading fontSize={"3xl"}>What happened?</Heading>
+
+        <Text textAlign={"center"} fontSize={"xl"}>
+          {modalDescription}
+        </Text>
+
+        <Heading fontSize={"3xl"}>Main tasks:</Heading>
+
+        <UnorderedList>
+          {taskList.map((task, key) => {
+            return <ListItem key={key}>{task}</ListItem>;
+          })}
+        </UnorderedList>
+      </VStack>
+    </Card>
   );
 };
--- src/pages/CoDesignPage/components/TimelineSection/TimelineSectionCompact.tsx
+++ src/pages/CoDesignPage/components/TimelineSection/TimelineSectionCompact.tsx
@@ -1,29 +1,49 @@
-import { Grid, Heading, VStack } from "@chakra-ui/react";
+import { Center, Grid, Heading, VStack, Text } from "@chakra-ui/react";
 import { timelineCardContents } from "./timelineCardContents";
 import { TimelineCardCompact } from "./TimelineCardCompact";
+import Xarrow from "react-xarrows";
+import { useState } from "react";
+import { Colours } from "../../../../colourScheme";
 
 export const TimelineSectionCompact = (): JSX.Element => {
+  const [arrow, setArrow] = useState(false);
+
   return (
     <Grid minH="93vh" position="relative">
-      <VStack mt="2rem">
-        <Heading textAlign={"center"}>
-          Timeline Compact. A list of card-like embeds in a stack. The embeds
-          have to be different that the large version though.
+      <VStack mt="2rem" mb="2rem">
+        <Heading textAlign={"center"}>Project Timeline</Heading>
+        <Heading textAlign={"center"} fontSize={"2xl"}>
+          (Compact Version)
         </Heading>
+        <Center mb="2rem">
+          <Text maxW="lg">Take a look at some of the activities we did!</Text>
+        </Center>
 
-        {timelineCardContents.map((card, index) => {
-          return (
-            <TimelineCardCompact
-              id={index}
-              cardImageURL={card.cardImageURL}
-              cardName={card.cardName}
-              modalDescription={card.modalDescription}
-              activityLocation={card.activityLocation}
-              activityDate={card.activityDate}
-              taskList={card.activityTasks}
-            />
-          );
-        })}
+        <VStack spacing={"5rem"}>
+          {timelineCardContents.map((card, index) => {
+            return (
+              <>
+                {arrow && timelineCardContents.length != index + 1 ? (
+                  <Xarrow
+                    start={`compactCard${index}`}
+                    end={`compactCard${index + 1}`}
+                    color={Colours.lightModeMainCol}
+                  />
+                ) : null}
+                <TimelineCardCompact
+                  id={`compactCard${index}`}
+                  cardImageURL={card.cardImageURL}
+                  cardName={card.cardName}
+                  modalDescription={card.modalDescription}
+                  activityLocation={card.activityLocation}
+                  activityDate={card.activityDate}
+                  taskList={card.activityTasks}
+                  onImageLoad={() => setArrow(true)}
+                />
+              </>
+            );
+          })}
+        </VStack>
       </VStack>
     </Grid>
   );
