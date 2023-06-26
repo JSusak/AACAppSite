0a995b1b5c2aa3299daae6ecf3fdb8b9525d4d2b
Josh <joshuasusak@gmail.com>
2023-06-26 12:50:37 +0100

    feat: create codesign explanation section - give user info on what it actually is before showing timeline

--- src/pages/CoDesignPage/CoDesignPage.tsx
+++ src/pages/CoDesignPage/CoDesignPage.tsx
@@ -1,11 +1,15 @@
 import { useMediaQuery } from "@chakra-ui/react";
 import { TimelineSection } from "./components/TimelineSection/TimelineSection";
 import { TimelineSectionCompact } from "./components/TimelineSection/TimelineSectionCompact";
+import { CodesignExplanationSection } from "./components/CodesignExplanationSection/CodesignExplanationSection";
 
 export const CoDesignPage = (): JSX.Element => {
   //Cutoff between a medium and large screen.
   const [isLargerThan991] = useMediaQuery("(min-width: 991px)");
   return (
-    <>{isLargerThan991 ? <TimelineSection /> : <TimelineSectionCompact />}</>
+    <>
+      <CodesignExplanationSection />
+      {isLargerThan991 ? <TimelineSection /> : <TimelineSectionCompact />}
+    </>
   );
 };
