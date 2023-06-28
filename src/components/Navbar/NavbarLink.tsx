5e642f2b3174c594ff02ecd25890f622528610f3
Josh <joshuasusak@gmail.com>
2023-06-28 15:02:47 +0100

    feat: add hamburger menu to navbar - refactor navlinks into array - add animation on drawer open

--- src/components/Navbar/NavbarLink.tsx
+++ src/components/Navbar/NavbarLink.tsx
@@ -2,13 +2,30 @@ import { useColorModeValue, Heading, Box } from "@chakra-ui/react";
 import { NavLink } from "react-router-dom";
 import { Colours } from "../../colourScheme";
 import "./navlink.scss";
+import { motion } from "framer-motion";
+
+const navLinkAnim = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+
+    transition: {
+      type: "spring",
+      duration: 1,
+    },
+  },
+};
 
 export const NavbarLink = ({
   linkTo,
   linkText,
+  onClick,
+  linkSize,
 }: {
   linkTo: string;
+  onClick?: () => void;
   linkText: string;
+  linkSize: string;
 }): JSX.Element => {
   return (
     <NavLink
@@ -17,28 +34,32 @@ export const NavbarLink = ({
         isActive ? "active" : "inactive"
       }
     >
-      <Box
-        px={3}
-        py={2}
-        rounded={"lg"}
-        borderRadius={"10px"}
-        textTransform={"uppercase"}
-        transition={"all 0.2s ease-in-out"}
-        _hover={{
-          textDecoration: "none",
-
-          bg: useColorModeValue(Colours.lightModeNavColHighlight, "red"),
-        }}
-      >
-        <Heading
-          as="h4"
-          size="md"
-          className="navName"
+      {" "}
+      <motion.span variants={navLinkAnim} onClick={onClick}>
+        <Box
+          px={3}
+          py={2}
+          rounded={"lg"}
+          borderRadius={"10px"}
+          textTransform={"uppercase"}
           transition={"all 0.2s ease-in-out"}
+          _hover={{
+            textDecoration: "none",
+
+            bg: useColorModeValue(Colours.lightModeNavColHighlight, "red"),
+          }}
         >
-          {linkText}
-        </Heading>
-      </Box>
+          <Heading
+            as="h4"
+            size={linkSize}
+            textAlign={"center"}
+            className="navName"
+            transition={"all 0.2s ease-in-out"}
+          >
+            {linkText}
+          </Heading>
+        </Box>
+      </motion.span>
     </NavLink>
   );
 };
