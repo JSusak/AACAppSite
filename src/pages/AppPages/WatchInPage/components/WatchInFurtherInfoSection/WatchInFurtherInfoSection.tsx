5e6ef41719e8394995057c509f640501a54a8674
Josh <joshuasusak@gmail.com>
2023-06-30 15:31:53 +0100

    chore: add responsive secondary header sizes in further

--- src/pages/AppPages/WatchInPage/components/WatchInFurtherInfoSection/WatchInFurtherInfoSection.tsx
+++ src/pages/AppPages/WatchInPage/components/WatchInFurtherInfoSection/WatchInFurtherInfoSection.tsx
@@ -2,12 +2,15 @@ import { Grid, Heading, VStack } from "@chakra-ui/react";
 import { VideoEmbed } from "../../../components/VideoEmbed/VideoEmbed";
 import { DownloadGrid } from "../../../components/DownloadGrid/DownloadGrid";
 import { watchInMaterials } from "../../../watchInOutDownloadMaterials";
+import { ResponsiveSizes } from "../../../../../responsiveSizes";
 
 export const WatchInFurtherInfoSection = (): JSX.Element => {
   return (
     <Grid minH="100vh" position="relative">
       <VStack mt="2rem" mb="2rem">
-        <Heading fontSize={"6xl"}>Further Info...</Heading>
+        <Heading fontSize={ResponsiveSizes.SecondaryHeaderSizes}>
+          Further Info...
+        </Heading>
         <VideoEmbed link="https://www.youtube.com/watch?v=15Rjg7Vbax0" />
         <DownloadGrid listOfDownloads={watchInMaterials} />
       </VStack>
--- src/pages/AppPages/WatchOutPage/components/WatchOutFurtherInfoSection/WatchOutFurtherInfoSection.tsx
+++ src/pages/AppPages/WatchOutPage/components/WatchOutFurtherInfoSection/WatchOutFurtherInfoSection.tsx
@@ -4,12 +4,15 @@ import { VideoEmbed } from "../../../components/VideoEmbed/VideoEmbed";
 import { DownloadGrid } from "../../../components/DownloadGrid/DownloadGrid";
 
 import { watchOutMaterials } from "../../../watchInOutDownloadMaterials";
+import { ResponsiveSizes } from "../../../../../responsiveSizes";
 
 export const WatchOutFurtherInfoSection = (): JSX.Element => {
   return (
     <Grid minH="100vh" position="relative">
       <VStack mt="2rem" mb="2rem">
-        <Heading fontSize={"6xl"}>Further Info...</Heading>
+        <Heading fontSize={ResponsiveSizes.SecondaryHeaderSizes}>
+          Further Info...
+        </Heading>
         <VideoEmbed link="https://www.youtube.com/watch?v=SNsdeS8VKg4" />
         <DownloadGrid listOfDownloads={watchOutMaterials} />
       </VStack>
