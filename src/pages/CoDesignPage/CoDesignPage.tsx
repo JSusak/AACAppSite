f857f8d51ae9dafa68e25c64ed210f1065aadb07
Josh <joshuasusak@gmail.com>
2023-07-03 13:25:47 +0100

    refactor: add page transition to codesign section

--- src/pages/CoDesignPage/CoDesignPage.tsx
+++ src/pages/CoDesignPage/CoDesignPage.tsx
@@ -2,14 +2,17 @@ import { useMediaQuery } from "@chakra-ui/react";
 import { TimelineSection } from "./components/TimelineSection/TimelineSection";
 import { TimelineSectionCompact } from "./components/TimelineSection/TimelineSectionCompact";
 import { CodesignExplanationSection } from "./components/CodesignExplanationSection/CodesignExplanationSection";
+import { PageTransition } from "../../PageTransition";
 
 export const CoDesignPage = (): JSX.Element => {
   //Cutoff between a medium and large screen.
   const [isLargerThan991] = useMediaQuery("(min-width: 991px)");
   return (
     <>
-      <CodesignExplanationSection />
-      {isLargerThan991 ? <TimelineSection /> : <TimelineSectionCompact />}
+      <PageTransition>
+        <CodesignExplanationSection />
+        {isLargerThan991 ? <TimelineSection /> : <TimelineSectionCompact />}
+      </PageTransition>
     </>
   );
 };
