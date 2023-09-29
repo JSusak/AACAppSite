42c19e6ff93f3d750c0ca8a2684ce576447db288
Josh <joshuasusak@gmail.com>
2023-09-29 12:26:53 +0100

    refactor: add text from ppt into timeline

--- src/pages/CoDesignPage/components/TimelineSection/timelineCardContents.ts
+++ src/pages/CoDesignPage/components/TimelineSection/timelineCardContents.ts
@@ -6,9 +6,9 @@ export const timelineCardContents = [
     cardImageURL: "./codesign/storyboarding.jpg",
     cardName: "Storyboarding",
     modalDescription:
-      "This is the text for the modal description of workshop 1.",
+      "Co-design workshop one explored participants’ communication experiences in different public contexts. We wanted our co-designers to feel empowered to share their unique stories about varying experiences with communication partners across multiple contexts. This consisted of using laminated tangible context cards (e.g., cinema, supermarket etc.). With cards, participants spontaneously shared stories when prompted by each card context. Followed by a group activity ranking each conversation context. We supported participants ranking frequency of visiting a context (Never to Daily) and they rated the degree of difficulty presented by communication within this setting (Easy to Hard).",
     activityLocation: "Southwark, London",
-    activityDate: "Oct. 2022",
+    activityDate: "Feb. 2023",
     activityTasks: ["task1", "task2", "task3"],
   },
 
@@ -17,41 +17,41 @@ export const timelineCardContents = [
     cardImageURL: "./codesign/lowfid.jpg",
     cardName: "Low-Fidelity Prototyping",
     modalDescription:
-      "This is the text for the modal description of workshop 2.",
+      "The second co-design workshop focused on supporting our co-designers to define and ideate low-fidelity solutions using craft materials. We prompted participants to envisage a ‘super’ tool that could assist them with these communication problems and enhance their pre-existing communication abilities. Amongst the small groups, the SLT/ researcher would mediate discussions and appropriately use closed ‘yes/no’ questioning to support prototyping activities (e.g., drawing, writing and cutting) of a high-volume of participants’ solutions. Low-fidelity wireframes were also successfully used by our codesigners to tangibly supplement their dialogue and expression.",
     activityLocation: "Aldwych, London",
-    activityDate: "Nov. 2022",
+    activityDate: "Feb. 2023",
     activityTasks: ["task1", "task2", "task3"],
   },
 
   //workshop 3,
   {
     cardImageURL: "./codesign/discussions.png",
-    cardName: "Further Discussions",
+    cardName: "Proof of concepts",
     modalDescription:
-      "This is the text for the modal description of workshop 3.",
+      "In co-design workshop 3, we built three proof of concept AAC smartwatch apps using codesigners wireframes. The apps contained most of the core features envisaged by participants. The apps were deliberately deployed on two devices of different sizes: an Apple Watch Series 8 (45mm) and Apple Watch SE 2 (40mm) to appropriately test font and icon size. Each participant with aphasia and the SLT tested the apps with support from a researcher. Furthermore, we performed light testing with Apple’s Assistive Touch activated amongst codesigners, especially those living with hemiplegic paralysis. We immediately held an open discussion about each app whether it achieved what our co-designers envisaged and suggested changes.",
     activityLocation: "Covent Garden, London",
-    activityDate: "Dec. 2022",
+    activityDate: "Feb. 2023",
     activityTasks: ["task1", "task2", "task3"],
   },
 
   //workshop 4
   {
     cardImageURL: "./codesign/highfid.png",
-    cardName: "High-Fidelity Prototyping",
+    cardName: "Experience Prototyping",
     modalDescription:
-      "This is the text for the modal description of workshop 4.",
+      "Following feedback and iteration of both smartwatch apps with our co-designers. Both the Watch Out and Watch In were evaluated via experience prototyping and role-playing. We wanted to determine the efficacy of our AAC in simulated ‘realworld’ scenarios, therefore we asked our participants to use both AAC smartwatch apps in a role-playing scenario with an actor e.g., ask for a seat from the actor. ",
     activityLocation: "Leicester Square, London",
-    activityDate: "Jan. 2023",
+    activityDate: "Mar. 2023",
     activityTasks: ["task1", "task2", "task3"],
   },
   //workshop 5
   {
     cardImageURL: "./codesign/experienceproto.png",
-    cardName: "Experience Prototyping",
+    cardName: "Focus Group",
     modalDescription:
-      "This is the text for the modal description of workshop 5.",
+      "The focus group was structured as follows: initially, participants arrived and seated themselves around a large central table, a researcher and SLT introduced and demonstrated both the Watch Out and Watch In apps using an Apple Watch Series 8 and Apple Watch SE. Sequentially, participants were presented with either app – encouraged to engage and test. After 15-20 minutes of usage, participants each completed a short feedback survey. Next, the SLT plus researcher facilitated a group discussion about the app, documenting participants’ comments and garnering consensus about potential future refinements to each app.",
     activityLocation: "Oxford Circus, London",
-    activityDate: "Feb. 2023",
+    activityDate: "Mar. 2023",
     activityTasks: ["task1", "task2", "task3"],
   },
 ];
