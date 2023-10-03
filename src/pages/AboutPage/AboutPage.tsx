41fc08b8a0ebd117dd2783b044639b1bde046da0
Josh <joshuasusak@gmail.com>
2023-10-03 17:27:13 +0100

    chore: remove our charity section from about page

--- src/pages/AboutPage/AboutPage.tsx
+++ src/pages/AboutPage/AboutPage.tsx
@@ -2,7 +2,6 @@ import { AboutTop } from "./components/AboutTop/AboutTop";
 import { ProjectOverview } from "./components/ProjectOverview/ProjectOverview";
 import { VisionAndValues } from "./components/VisionAndValues/VisionAndValues";
 import { InteractiveSection } from "./components/InteractiveSection/InteractiveSection";
-import { OurCharity } from "./components/OurCharity/OurCharity";
 import { PublicationSection } from "./components/PublicationSection/PublicationSection";
 import { PageTransition } from "../../PageTransition";
 
@@ -23,9 +22,6 @@ export const AboutPage: React.FC = (): JSX.Element => {
 
         {/* Academic Publications section */}
         <PublicationSection />
-
-        {/* Aphasia Reconnectsection */}
-        <OurCharity />
       </PageTransition>
     </>
   );
